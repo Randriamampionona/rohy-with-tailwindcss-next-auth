043e187ae147315ad0f165ab443d1bc41cfd35ac
Randriamampionona <105777560+Randriamampionona@users.noreply.github.com>
2022-08-25 16:28:12 +0300

    test if bug fixed

--- components/Authorization/SignIn/OtherMethods.jsx
+++ components/Authorization/SignIn/OtherMethods.jsx
@@ -1,9 +1,7 @@
-import { useSession, signIn } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import { RiGoogleFill, RiFacebookFill, RiGithubFill } from "react-icons/ri";
 
 const OtherMethods = () => {
-	const { data: session } = useSession();
-
 	return (
 		<div className="flex space-x-4">
 			<div className="iconBtn group hover:scale-[1.1] ease">
